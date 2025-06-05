9d4f325d7438f3a6d9603e31f233ad5f3df8eed6
imsoyfaygo <soyyak28@gmail.com>
2025-06-05 17:59:19 +0200

    Migrations

---
+++ database/migrations/2025_06_05_155902_create_favorites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('book_id');
+            $table->timestamps();
+            
+            $table->unique(['user_id', 'book_id'], 'favorites_user_id_book_id_unique');
+            $table->foreign('user_id', 'favorites_user_id_foreign')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
