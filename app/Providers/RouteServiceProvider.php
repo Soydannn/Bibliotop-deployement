741cf9a959f735e8003203ebd0fca85ebcc25b4b
imsoyfaygo <soyyak28@gmail.com>
2025-06-05 17:21:18 +0200

    Hotfix

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public const HOME = '/';
+
+    // Si tu veux, tu peux copier le contenu complet du RouteServiceProvider de Laravel pour éviter des bugs
+}
